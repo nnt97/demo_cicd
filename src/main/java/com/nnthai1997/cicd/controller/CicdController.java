4778f37f86c0a6e26d89641b5ddb5385373a0d0f
Nguyen Ngoc Thai <thainguyenngocnbk@gmail.com>
2019-05-27 10:27:09 +0700

    Update CicdController.java

--- src/main/java/com/nnthai1997/cicd/controller/CicdController.java
+++ src/main/java/com/nnthai1997/cicd/controller/CicdController.java
@@ -52,6 +52,7 @@ public class CicdController {
 	@PostMapping("/githook")
 	public void receiveHookGit() {
 		System.out.println("111111111111111111");
+		System.out.println("22222222222222222222222");
 	}
 	
 	private List<Repos> getListRepos(String accessToken) {
