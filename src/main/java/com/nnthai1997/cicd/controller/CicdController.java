96fab6cc28adbb95610d83ce98ff1127d9e6543f
Nguyen Ngoc Thai <thainguyenngocnbk@gmail.com>
2019-05-27 10:16:16 +0700

    Update CicdController.java

--- src/main/java/com/nnthai1997/cicd/controller/CicdController.java
+++ src/main/java/com/nnthai1997/cicd/controller/CicdController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
